35ddc50a8b3f797aaad351293ed27e67088e4611
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-06-19 15:07:43 -0600

    broken link to db

--- app.py
+++ app.py
@@ -28,16 +28,115 @@ class IncidentForm(db.Model):
     user = db.relationship('User',
         backref=db.backref('IncidentForms', lazy=True))
 
+    def __init__(self, author, ppe, conditions, equipment, others, meds, description):
+        self.author = author
+        self.ppe = ppe
+        self.conditions = conditions
+        self.equipment = equipment
+        self.others = others
+        self.meds = meds
+        self.description = description
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
     user_name = db.Column(db.String(50), nullable=False)
     department = db.Column(db.Integer, nullable=False)
     admin = db.Column(db.Boolean, nullable=False)
 
+    def __init__(self, user_name, department, admin):
+        self.user_name = user_name
+        self.department = department
+        self.admin = admin
+
+class IncidentFormSchema(ma.Schema):
+    class Meta:
+        fields = ("id",
+            "pub_date",
+            "author",
+            "ppe",
+            "conditions",
+            "equipment",
+            "others",
+            "meds",
+            "description",
+            "user"
+        )
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = (
+            "id",
+            "user_name",
+            "department",
+            "admin"
+        )
+            
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+incidentForm_schema = IncidentFormSchema()
+incidentForms_schema = IncidentFormSchema(many=True)
 
 @app.route("/")
-def hello():
+def hello_world():
     return "Hello, World!"
+    # output = incidentForm_schema.dump().data
+    # return jsonify({'user' : output})
+
+@app.route("/incidentForm", methods=["POST"])
+def add_incidentForm():
+    author = request.json["author"]
+    ppe = request.json["ppe"],
+    conditions = request.json["conditions"],
+    equipment = request.json["equipment"],
+    others = request.json["others"],
+    meds = request.json["meds"],
+    description = request.json["description"],
+
+    new_incidentForm = IncidentForm(
+            author,
+            ppe,
+            conditions,
+            equipment,
+            others,
+            meds,
+            description)
+
+    db.session.add(new_incidentForm)
+    db.session.commit()
+
+    incidentForm = IncidentForm.query.get(new_incidentForm.id)
+    return incidentForm_schema.jsonify(incidentForm)
+
+@app.route("/user", methods=["POST"])
+def add_user():
+  user_name = request.json["user_name"]
+  department = request.json["department"]
+  admin = request.json["admin"]
+
+  new_user = User(user_name, department, admin)
+
+  db.session.add(new_user)
+  db.session.commit()
+
+  user = User.query.get(new_user.id)
+  return user_schema.jsonify(user)
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    all_users = User.query.all()
+    result = users_schema.dump(all_users)
+
+    return jsonify(result)
+
+@app.route("/incidentForms", methods=["GET"])
+def get_incident_forms():
+    all_incident_forms = IncidentForm.query.all()
+    result = incidentForms_schema.dump(all_incident_forms)
+
+    return jsonify(result)
+
 
 if __name__ == "__main__":
     app.debug = True
