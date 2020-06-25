607c19222338e4c4b38624db6bd3b786bd8feb5d
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-06-25 13:32:32 -0600

    fixed procfile

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 heroku = Heroku(app)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
